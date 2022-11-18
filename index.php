c44e9e779ae5f73c1f1a40ffb67e985f86f611d0
jackiewin <empor0901@gmail.com>
2022-11-18 16:41:06 +0800

    Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 // 如「資料表名」= actions，則「模組物件」= Actions
 use Xmf\Request;
 use XoopsModules\Tadtools\Utility;
-use XoopsModules\首字大寫模組目錄\模組物件;
+use XoopsModules\Win_singup\Win_singup_actions;
 
 /*-----------引入檔案區--------------*/
 require_once __DIR__ . '/header.php';
-$GLOBALS['xoopsOption']['template_main'] = '模組目錄_index.tpl';
+$GLOBALS['xoopsOption']['template_main'] = 'win_signup_index.tpl';
 require_once XOOPS_ROOT_PATH . '/header.php';
 
 /*-----------變數過濾----------*/
@@ -18,41 +18,41 @@ $id = Request::getInt('id');
 switch ($op) {
 
     //新增表單
-    case '資料表名_create':
-        模組物件::create();
+    case 'win_singup_actions_create':
+        Win_singup_actions::create();
         break;
 
     //新增資料
-    case '資料表名_store':
-        $id = 模組物件::store();
+    case 'win_singup_actions_store':
+        $id = Win_singup_actions::store();
         header("location: {$_SERVER['PHP_SELF']}?id=$id");
         exit;
 
     //修改用表單
-    case '資料表名_edit':
-        模組物件::create($id);
-        $op = '資料表名_create';
+    case 'win_singup_actions_edit':
+        Win_singup_actions::create($id);
+        $op = 'win_singup_actions_create';
         break;
 
     //更新資料
-    case '資料表名_update':
-        模組物件::update($id);
+    case 'win_singup_actions_update':
+        Win_singup_actions::update($id);
         header("location: {$_SERVER['PHP_SELF']}?id=$id");
         exit;
 
     //刪除資料
-    case '資料表名_destroy':
-        模組物件::destroy($id);
+    case 'win_singup_actions_destroy':
+        Win_singup_actions::destroy($id);
         header("location: {$_SERVER['PHP_SELF']}");
         exit;
 
     default:
         if (empty($id)) {
-            模組物件::index();
-            $op = '資料表名_index';
+            Win_singup_actions::index();
+            $op = 'win_singup_actions_index';
         } else {
-            模組物件::show($id);
-            $op = '資料表名_show';
+            Win_singup_actions::show($id);
+            $op = 'win_singup_actions_show';
         }
         break;
 }
@@ -63,5 +63,5 @@ switch ($op) {
 unset($_SESSION['api_mode']);
 $xoopsTpl->assign('toolbar', Utility::toolbar_bootstrap($interface_menu));
 $xoopsTpl->assign('now_op', $op);
-$xoTheme->addStylesheet(XOOPS_URL . '/modules/模組目錄/css/module.css');
+$xoTheme->addStylesheet(XOOPS_URL . '/modules/win_singup/css/module.css');
 require_once XOOPS_ROOT_PATH . '/footer.php';
