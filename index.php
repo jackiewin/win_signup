6bfbf4d59624da58c9447d04207a4950815e0b2b
jackiewin <empor0901@gmail.com>
2023-01-25 07:44:05 +0800

    new file:   README 	new file:   README.md 	new file:   admin/about.php 	new file:   admin/footer.php 	new file:   admin/header.php 	new file:   admin/index.html 	new file:   admin/index.php 	new file:   admin/main.php 	new file:   admin/menu.php 	new file:   blocks/index.html 	new file:   class/Helper.php 	new file:   class/Update.php 	new file:   class/Win_signup_actions.php 	new file:   class/index.html 	new file:   "class/\346\250\241\347\265\204\347\211\251\344\273\266.php" 	new file:   css/admin.css 	new file:   css/index.html 	new file:   css/module.css 	new file:   docs/credits.txt 	new file:   docs/index.html 	new file:   docs/install.txt 	new file:   docs/lang_diff.txt 	new file:   docs/licence.txt 	new file:   docs/readme.txt 	new file:   function.php 	new file:   header.php 	new file:   images/admin/about.png 	new file:   images/admin/button.png 	new file:   images/admin/help.png 	new file:   images/admin/home.png 	new file:   images/admin/index.html 	new file:   images/admin/xoopsmicrobutton.gif 	new file:   images/icons/index.html 	new file:   images/icons/off.png 	new file:   images/icons/on.png 	new file:   images/index.html 	new file:   images/logo.png 	new file:   include/index.html 	new file:   include/onInstall.php 	new file:   include/onUninstall.php 	new file:   include/onUpdate.php 	new file:   include/search.php 	new file:   index.php 	new file:   interface_menu.php 	new file:   language/english/admin.php 	new file:   language/english/changelog.txt 	new file:   language/english/help/help.html 	new file:   language/english/help/index.html 	new file:   language/english/index.html 	new file:   language/english/main.php 	new file:   language/english/modinfo.php 	new file:   language/index.html 	new file:   language/tchinese_utf8/admin.php 	new file:   language/tchinese_utf8/changelog.txt 	new file:   language/tchinese_utf8/help/help.html 	new file:   language/tchinese_utf8/help/index.html 	new file:   language/tchinese_utf8/index.html 	new file:   language/tchinese_utf8/main.php 	new file:   language/tchinese_utf8/modinfo.php 	new file:   preloads/autoloader.php 	new file:   preloads/core.php 	new file:   preloads/index.html 	new file:   sql/index.html 	new file:   sql/mysql.sql 	new file:   templates/blocks/index.html 	new file:   templates/index.html 	new file:   templates/op_win_signup_actions_index.tpl 	new file:   templates/win_signup_admin.tpl 	new file:   templates/win_signup_index.tpl 	new file:   xoops_version.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+// 如「模組目錄」= signup，則「首字大寫模組目錄」= Signup
+// 如「資料表名」= actions，則「模組物件」= Actions
+use Xmf\Request;
+use XoopsModules\Tadtools\Utility;
+use XoopsModules\Win_signup\Win_signup_actions;
+
+/*-----------引入檔案區--------------*/
+require_once __DIR__ . '/header.php';
+$GLOBALS['xoopsOption']['template_main'] = 'win_signup_index.tpl';
+require_once XOOPS_ROOT_PATH . '/header.php';
+
+/*-----------變數過濾----------*/
+$op = Request::getString('op');
+$id = Request::getInt('id');
+
+/*-----------執行動作判斷區----------*/
+switch ($op) {
+
+    //新增表單
+    case 'win_signup_actions_create':
+        Win_signup_actions::create();
+        break;
+
+    //新增資料
+    case 'win_signup_actions_store':
+        $id = Win_signup_actions::store();
+        header("location: {$_SERVER['PHP_SELF']}?id=$id");
+        exit;
+
+    //修改用表單
+    case 'win_signup_actions_edit':
+        Win_signup_actions::create($id);
+        $op = 'win_signup_actions_create';
+        break;
+
+    //更新資料
+    case 'win_signup_actions_update':
+        Win_signup_actions::update($id);
+        header("location: {$_SERVER['PHP_SELF']}?id=$id");
+        exit;
+
+    //刪除資料
+    case 'win_signup_actions_destroy':
+        Win_signup_actions::destroy($id);
+        header("location: {$_SERVER['PHP_SELF']}");
+        exit;
+
+    default:
+        if (empty($id)) {
+            Win_signup_actions::index();
+            $op = 'win_signup_actions_index';
+        } else {
+            Win_signup_actions::show($id);
+            $op = 'win_signup_actions_show';
+        }
+        break;
+}
+
+/*-----------function區--------------*/
+
+/*-----------秀出結果區--------------*/
+unset($_SESSION['api_mode']);
+$xoopsTpl->assign('toolbar', Utility::toolbar_bootstrap($interface_menu));
+$xoopsTpl->assign('now_op', $op);
+$xoTheme->addStylesheet(XOOPS_URL . '/modules/win_signup/css/module.css');
+require_once XOOPS_ROOT_PATH . '/footer.php';
---
+++ preloads/core.php
@@ -0,0 +1,15 @@
+<?php
+defined('XOOPS_ROOT_PATH') || die('Restricted access.');
+
+class Win_signupCorePreload extends XoopsPreloadItem
+{
+    // to add PSR-4 autoloader
+
+    /**
+     * @param $args
+     */
+    public static function eventCoreIncludeCommonEnd($args)
+    {
+        require __DIR__ . '/autoloader.php';
+    }
+}
